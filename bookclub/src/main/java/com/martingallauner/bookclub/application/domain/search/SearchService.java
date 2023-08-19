9b390c4edaeb14ae5ee64737fe1a5c58f23b08ea
Martin Gallauner <martin.gallauner@pm.me>
2023-08-19 08:00:28 +0200

    Introduces new port (in) interface for the use case of searching for books.

--- bookclub/src/main/java/com/martingallauner/bookclub/application/domain/search/SearchService.java
+++ bookclub/src/main/java/com/martingallauner/bookclub/application/domain/search/SearchService.java
@@ -3,17 +3,20 @@ package com.martingallauner.bookclub.application.domain.search;
 import com.martingallauner.bookclub.application.domain.book.Book;
 import com.martingallauner.bookclub.application.domain.book.BookService;
 import com.martingallauner.bookclub.application.domain.user.User;
+import com.martingallauner.bookclub.application.port.in.SearchBookUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
-public class SearchService {
+public class SearchService implements SearchBookUseCase {
 
     private final BookService bookService;
 
+    @Override
     public Set<User> searchBooks(String isbn) {
         Book book = bookService.getBookByIsbn(isbn);
         return book.getUsers();
--- bookclub/src/main/java/com/martingallauner/bookclub/application/port/in/SearchBookUseCase.java
+++ bookclub/src/main/java/com/martingallauner/bookclub/application/port/in/SearchBookUseCase.java
@@ -1,10 +1,11 @@
 package com.martingallauner.bookclub.application.port.in;
 
-import com.martingallauner.bookclub.application.domain.user.UserResponse;
 
-import java.util.List;
+import com.martingallauner.bookclub.application.domain.user.User;
+
+import java.util.Set;
 
 public interface SearchBookUseCase {
 
-    List<UserResponse> searchBooks(String isbn);
+    Set<User> searchBooks(String isbn);
 }
