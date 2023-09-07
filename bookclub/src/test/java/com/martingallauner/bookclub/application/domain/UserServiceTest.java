8866e40b85cec3c8a3ab34713c22b906ee30ba1d
Martin Gallauner <martin.gallauner@pm.me>
2023-09-07 08:01:03 +0200

    removes empty tests

--- bookclub/src/test/java/com/martingallauner/bookclub/application/domain/UserServiceTest.java
+++ bookclub/src/test/java/com/martingallauner/bookclub/application/domain/UserServiceTest.java
@@ -6,6 +6,7 @@ import com.martingallauner.bookclub.application.TimeService;
 import com.martingallauner.bookclub.application.domain.model.UserModel;
 import com.martingallauner.bookclub.application.port.out.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,6 +32,7 @@ class UserServiceTest {
     @Mock
     private TimeService timeService;
 
+    @DisplayName("Get user by id, happy path.")
     @Test
     void getUserById() {
         when(userRepository.getReferenceById(1L))
@@ -39,7 +41,7 @@ class UserServiceTest {
                 .name("Test User")
                 .books(Set.of(BookEntity.builder().title("Test Title").author("Test Author").build()))
                 .connections(List.of(UserEntity.builder().name("Test Friend").build()))
-                .createdAt(LocalDateTime.of(2023, 9, 07, 07, 39))
+                .createdAt(LocalDateTime.of(2023, 9, 7, 7, 39))
                 .build());
 
         UserModel user = userService.getUserById(1L);
@@ -48,17 +50,7 @@ class UserServiceTest {
         Assertions.assertEquals("Test User", user.getName());
         Assertions.assertEquals("Test Title", user.getBooks().get(0).getTitle());
         Assertions.assertEquals("Test Author", user.getBooks().get(0).getAuthor());
+        Assertions.assertEquals(LocalDateTime.of(2023, 9, 7, 7, 39), user.getCreatedAt());
     }
 
-    @Test
-    void createUser() {
-    }
-
-    @Test
-    void addBook() {
-    }
-
-    @Test
-    void addConnection() {
-    }
 }
