d8001855193b9e8ea57c3e3c752773b3b0b53de6
Martin Gallauner <martin.gallauner@pm.me>
2023-09-08 10:30:45 +0200

    Adds input validation

--- bookclub/src/test/java/com/martingallauner/bookclub/application/port/in/request/CreateUserRequestTest.java
+++ bookclub/src/test/java/com/martingallauner/bookclub/application/port/in/request/CreateUserRequestTest.java
@@ -1,17 +1,17 @@
 package com.martingallauner.bookclub.application.port.in.request;
 
 import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateUserRequestTest {
 
+    @DisplayName("Testing input validation when creating a new user.")
     @Test
     void createInvalidUser() {
-
         assertThrows(ConstraintViolationException.class, () -> new CreateUserRequest(null, null));
-
+        assertThrows(ConstraintViolationException.class, () -> new CreateUserRequest("", ""));
     }
-
 }
