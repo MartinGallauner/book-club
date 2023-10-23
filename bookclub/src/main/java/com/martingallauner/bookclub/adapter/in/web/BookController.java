76866ee3a6086a1ce5d990c86ed9e71b18a67343
Martin Gallauner <martin.gallauner@pm.me>
2023-10-23 09:21:40 +0200

    adds swagger annoations

--- bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/BookController.java
+++ bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/BookController.java
@@ -2,6 +2,7 @@ package com.martingallauner.bookclub.adapter.in.web;
 
 import com.martingallauner.bookclub.application.port.in.FindBookUseCase;
 import com.martingallauner.bookclub.application.port.in.response.BookResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ public class BookController {
 
     private final FindBookUseCase findBookUseCase;
 
+    @Operation(summary = "Get book by isbn")
     @GetMapping("/{isbn}")
     public BookResponse getBook(@PathVariable String isbn) {
         return findBookUseCase.getBook(isbn).toResponse();
--- bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/user/UserController.java
+++ bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/user/UserController.java
@@ -7,6 +7,7 @@ import com.martingallauner.bookclub.application.port.in.GetUserUseCase;
 import com.martingallauner.bookclub.application.port.in.request.ConnectUserRequest;
 import com.martingallauner.bookclub.application.port.in.request.CreateUserRequest;
 import com.martingallauner.bookclub.application.port.in.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +22,21 @@ public class UserController {
     private final AddConnectionUseCase addConnectionUseCase;
 
 
+    @Operation(summary = "Get user by id")
     @GetMapping("/{id}")
     public UserResponse getUserById(@PathVariable Long id) {
         UserModel userEntity = getUserUseCase.getUserById(id);
         return userEntity.toResponse();
     }
 
+    @Operation(summary = "Create user")
     @PostMapping
     public UserResponse createUser(@RequestBody @Valid CreateUserRequest createUserRequest) {
         UserModel user = createUserUseCase.createUser(createUserRequest);
         return user.toResponse();
     }
 
+    @Operation(summary = "Connect users")
     @PostMapping("/connect")
     public void connectUser(@RequestBody ConnectUserRequest connectUserRequest) {
         addConnectionUseCase.addConnection(connectUserRequest);
