79bc2aed1b16b6a602ecced70a47e0459d1f48ee
Martin Gallauner <martin.gallauner@pm.me>
2023-10-24 10:48:45 +0200

    swaggerdoc annotations

--- bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/SearchController.java
+++ bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/SearchController.java
@@ -3,6 +3,7 @@ package com.martingallauner.bookclub.adapter.in.web;
 import com.martingallauner.bookclub.application.domain.model.UserModel;
 import com.martingallauner.bookclub.application.port.in.SearchBookInNetworkUseCase;
 import com.martingallauner.bookclub.application.port.in.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ public class SearchController {
 
     private final SearchBookInNetworkUseCase searchBookUseCase;
 
-
+    @Operation(summary = "Search for a book in the network")
     @GetMapping("/{isbn}")
     public List<UserResponse> searchBooks(@PathVariable String isbn) {
         Set<UserModel> users = searchBookUseCase.searchBooks(isbn);
--- bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/assigned/AssignmentController.java
+++ bookclub/src/main/java/com/martingallauner/bookclub/adapter/in/web/assigned/AssignmentController.java
@@ -4,6 +4,7 @@ import com.martingallauner.bookclub.application.domain.model.BookModel;
 import com.martingallauner.bookclub.application.port.in.AssignBookUseCase;
 import com.martingallauner.bookclub.application.port.in.GetAssignedBooksUseCase;
 import com.martingallauner.bookclub.application.port.in.request.AssignmentRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,11 +19,13 @@ public class AssignmentController {
 
     private final GetAssignedBooksUseCase getAssignedBooksUseCase;
 
+    @Operation(summary = "Assign a book to a user")
     @PostMapping("/assign")
     public void assign(@RequestBody AssignmentRequest assignment) {
         assignBookUseCase.assign(assignment);
     }
 
+    @Operation(summary = "Get all books assigned to a user")
     @GetMapping("/user/{userId}")
     public AssignedBooksResponse getBooksByUserId(@PathVariable Long userId) {
         List<BookModel> assignedBooks = getAssignedBooksUseCase.getAssignedBooks(userId);
